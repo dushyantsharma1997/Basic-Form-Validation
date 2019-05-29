30e7bb6bb16a8f5e7839587f45a2fcbcfe882ef8
Dushyant Sharma <dushyant.sharma1997@gmail.com>
2019-05-29 11:30:20 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,6 +1,27 @@
 var text = new String();
-var details = '{"data":[{"username":"Harry","password":"potter","first_name":"Harry James","last_name":"Potter","Age":"19"}]}';
-obj = JSON.parse(details);
+var req = new XMLHttpRequest();
+req.open("GET", "data.json", true);
+req.onload = function (e) 
+{
+    if (req.readyState === 4) 
+    {
+        if (req.status === 200) 
+        {
+            console.log(req.responseText);
+            obj = JSON.parse(req.responseText);
+        } 
+        else 
+        {
+            console.error(req.statusText);
+        }
+    }
+};
+req.onerror = function (e) 
+{
+  console.error(req.statusText);
+};
+req.send(null);
+
 
 function mysubmit(){
     var x=document.forms["Login"]["name"].value;
@@ -27,7 +48,7 @@ function mysubmit(){
     }
 
 
-    function generate_captcha()
+function generate_captcha()
 {
     var a,i;
     a="abcdefghijklmnopqrstuvwxyz";
@@ -38,17 +59,18 @@ function mysubmit(){
     document.getElementById("captcha").innerHTML = text;
 }
 
+
 function detail(){
-    document.getElementById("temp1").innerHTML = "<h1> Welcome " + obj.data[0].first_name + " " +
-                                                obj.data[0].last_name + "</h1>";
+    document.getElementById("temp1").innerHTML = "<h1> Welcome " + obj.first_name + " " +
+                                                obj.last_name + "</h1>";
     document.getElementById("temp1").innerHTML += "<br><br>"
             
     var temp="<table align=\"center\" width=\"400px\" height=\"200px\">" + 
-                "<tr><td> The username is </td> <td> " + obj.data[0].username + "</td></tr>" +
-                "<tr><td> The password is </td> <td>" + obj.data[0].password + "</td></tr>" +
-                "<tr><td> The first name is </td> <td>" +obj.data[0].first_name + "</td></tr>"+
-                "<tr><td> The last name is </td> <td>" + obj.data[0].last_name + "</td></tr>"+
-                "<tr><td> The age is </td> <td>" + obj.data[0].Age + " years </td></tr>"
+                "<tr><td> The username is </td> <td> " + obj.username + "</td></tr>" +
+                "<tr><td> The password is </td> <td>" + obj.password + "</td></tr>" +
+                "<tr><td> The first name is </td> <td>" +obj.first_name + "</td></tr>"+
+                "<tr><td> The last name is </td> <td>" + obj.last_name + "</td></tr>"+
+                "<tr><td> The age is </td> <td>" + obj.Age + " years </td></tr>"
                 "</table>";
     document.getElementById("temp2").innerHTML += temp;
 }
